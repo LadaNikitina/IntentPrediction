81bb4271f6ee203f46800c505db8cf118056f7fb
Ledneva Daria <da.led@mail.ru>
2023-06-23 23:00:18 +0300

    Update and rename MultiWoz/encoder_distilroberta.py to MultiWoz/Encoder/Encoder.py

--- MultiWoz/Encoder/Encoder.py
+++ MultiWoz/Encoder/Encoder.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-
 from datasets import load_dataset
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
@@ -31,90 +25,49 @@ import torch.nn.functional as F
 import dgl.nn.pytorch as dglnn
 import torch.nn as nn
 
-
-# In[8]:
-
-
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="3"
 print(torch.cuda.device_count())
 
-
-# In[9]:
-
-
-first_num_clusters = 200
+first_num_clusters = 200 # set the number of clusters
 second_num_clusters = 30
 
-
-# In[10]:
-
-
 import sys
-sys.path.insert(1, '/cephfs/home/ledneva/multiwoz/common_utils/')
-
-
-# In[11]:
-
+sys.path.insert(1, '/multiwoz/utils/') # set the correct path to the utils dir
 
 from preprocess import Clusters, get_accuracy_k, get_all_accuracy_k
-
 
 num_iterations = 3
 
 file = open("encoder_distilroberta.txt", "w")
 
-# In[3]:
-
 for iteration in range(num_iterations):
-# In[12]:
-
-
-    path = "/cephfs/home/ledneva/multiwoz/distilroberta_embeddings.csv"
+    # set the correct path to the preprocessed embeddings
+    path = "/multiwoz/distilroberta_embeddings.csv"
     clusters = Clusters(first_num_clusters, second_num_clusters, path)
     clusters.form_clusters()
-
-
-    # In[13]:
-
-
+    
     from sentence_transformers import SentenceTransformer
 
     model = SentenceTransformer('sentence-transformers/all-distilroberta-v1')
     model = model.to('cuda')
-
-
-    # In[14]:
-
 
     train_user_utterances = clusters.train_user_df['utterance'].tolist()
     train_system_utterances = clusters.train_system_df['utterance'].tolist()
     train_user_clusters = clusters.train_user_df['cluster'].tolist()
     train_system_clusters = clusters.train_system_df['cluster'].tolist()
 
-
-    # In[15]:
-
-
     batches_train_user_utterances = np.array_split(train_user_utterances, 5000)
     batches_train_system_utterances = np.array_split(train_system_utterances, 5000)
-
-
-    # In[16]:
-
 
     from tqdm import tqdm
 
     train_user_embeddings = np.concatenate([model.encode(train_user_utterances)
-                                for train_user_utterances in tqdm(batches_train_user_utterances)])
+                                            for train_user_utterances in tqdm(batches_train_user_utterances)])
 
 
-    train_system_embeddings = np.concatenate([model.encode(train_system_utterances) for train_system_utterances in tqdm(batches_train_system_utterances)])
-
-
-
-    # In[22]:
-
+    train_system_embeddings = np.concatenate([model.encode(train_system_utterances)
+                                              for train_system_utterances in tqdm(batches_train_system_utterances)])
 
     user_metric = {1 : [], 3 : [], 5 : [], 10 : []}
     system_metric = {1 : [], 3 : [], 5 : [], 10 : []}
@@ -187,10 +140,6 @@ for iteration in range(num_iterations):
             user_metric[k].append(np.mean(user_utterence_metric[k])) 
             system_metric[k].append(np.mean(system_utterence_metric[k])) 
 
-
-    # In[23]:
-
-
     file.write("USER METRIC\n")
 
     for k in [1, 3, 5, 10]:
@@ -205,10 +154,3 @@ for iteration in range(num_iterations):
 
     for k in [1, 3, 5, 10]:
         file.write(f"Acc@{k}: {(np.mean(system_metric[k]) + np.mean(user_metric[k])) / 2}\n")
-
-
-    # In[ ]:
-
-
-
-
